81b59fb2b42d2d17089d820d20363dc8750890de
Developer@Tensorkart.com <Developer@Tensorkart.com>
2024-07-02 07:11:21 +0530

    added support for performing OCR on an image

--- DescriptionDialog.py
+++ DescriptionDialog.py
@@ -1,6 +1,7 @@
 # DescriptionDialog.py
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QTextEdit, QPushButton
 import os
+from paddleocr import PaddleOCR,draw_ocr
 
 class DescriptionDialog(QDialog):
     def __init__(self, image_path, parent=None):
@@ -22,9 +23,24 @@ class DescriptionDialog(QDialog):
 
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.submit)
+
+        self.ocr_button = QPushButton("Perform OCR")
+        self.ocr_button.clicked.connect(self.doOCR)
         layout.addWidget(self.submit_button)
+        layout.addWidget(self.ocr_button)
 
         self.setLayout(layout)
+    def doOCR(self):
+        image = self.image_path
+        print(f"Performing OCR on {image}")
+        ocr = PaddleOCR(use_angle_cls=True, lang='en')
+        result = ocr.ocr(image, cls=True)
+        ocr_text = '\n'.join([line[1][0] for line in result[0]])
+        txt_filename = os.path.splitext(image)[0] + '.txt'
+        with open(txt_filename, 'w') as txt_file:
+            txt_file.write(ocr_text)
+        print(f'OCR completed for {image}. Text saved to {txt_filename}.')
+       
 
     def submit(self):
         description = self.text_edit.toPlainText()
